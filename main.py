722e060424f94434c6cb3558981e002b74860a81
Carlos Albeiro Galeano Gomez <99480165+Carlos0329@users.noreply.github.com>
2022-03-18 07:29:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ from delete import delete
 from actualizar import *
 from flask import Flask
 
+
 def index():
     opc=int(input("-----Bienvenido-----\n--Menu de opciones--\n-Seleccione 1 si desea logearse\n-Seleccione 2 si desea registrarse\n-Seleccione 3 si desea eliminar \n-Seleccione 4 si desea actualizar\n: "))
     if opc == 1:
         correo=input("Ingrese su correo: ")
         contrasena=input("Ingrese su contraseña: ")
         return login(correo,contrasena)
-
     elif opc == 2:
         correo = input("Ingrese su correo:     ")
         contrasena = input("Ingrese su contraseña: ")
         nombre = input("Ingrese su nombre:     ")
         rol = int(input("Ingrese el rol:        "))
         return registro(correo, contrasena, nombre, rol)
-
     elif opc == 3:
         correo=input("Ingrese el correo del usuario a eliminar: ")
         return delete(correo)
